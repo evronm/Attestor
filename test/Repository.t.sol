f03a571f1b7bba6f8e20986e5617014709171b0a
Mike Evron <evronm@gmail.com>
2022-05-25 16:10:52 -0700

    some more tests

--- test/Repository.t.sol
+++ test/Repository.t.sol
@@ -17,11 +17,26 @@ contract RepositoryTest is Test {
         repository.create_attestation("test",props);
         repository.create_attestation("test",props);
     }
-    function testAttestationWithProps() public {
+    function testAttestationWithProp() public {
         props.push(prop("name","test"));
         repository.create_attestation("test",props);
         assertEq(1,repository.attestations().length);
         assert(Utils.compareStrings("name", repository.attestations()[0].props[0].key));
+        assert(! Utils.compareStrings("ame", repository.attestations()[0].props[0].key));
         assert(Utils.compareStrings("test", repository.attestations()[0].props[0].val));
+        assert(! Utils.compareStrings("tst", repository.attestations()[0].props[0].val));
+    }
+    function testAttestationWithProps() public {
+        props.push(prop("name","test"));
+        props.push(prop("name2","test2"));
+        props.push(prop("name3","test3"));
+        repository.create_attestation("test",props);
+        assertEq(1,repository.attestations().length);
+        assert(Utils.compareStrings("name", repository.attestations()[0].props[0].key));
+        assert(Utils.compareStrings("test", repository.attestations()[0].props[0].val));
+        assert(Utils.compareStrings("name2", repository.attestations()[0].props[1].key));
+        assert(Utils.compareStrings("test2", repository.attestations()[0].props[1].val));
+        assert(Utils.compareStrings("name3", repository.attestations()[0].props[2].key));
+        assert(Utils.compareStrings("test3", repository.attestations()[0].props[2].val));
     }
 }
