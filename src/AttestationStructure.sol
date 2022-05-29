e068f591430fe2717281f56a5ece08b3c518ecfb
Mike Evron <evronm@gmail.com>
2022-05-29 13:48:16 -0700

    new props structure works

--- src/AttestationStructure.sol
+++ src/AttestationStructure.sol
@@ -2,15 +2,29 @@
 pragma solidity ^0.8.13;
 import "./Lib.sol";
 
-contract StructuredRepository {
+contract AttestationStructure {
   string[] private _prop_names;
-  attestation_instance[] private _attestation_instances;
+  attestation_instance[] private _instances;
 
   mapping (address => mapping(uint => address[])) private _attestations_about;
   mapping (address => mapping(uint => address[])) private _attestations_by;
   mapping (address => uint) private _num_attestations_about;
   mapping (address => uint) private _num_attestations_by;
+
   constructor(string[] memory prop_names) {
     _prop_names=prop_names;
   }
+  function new_instance(string[] memory vals) public {
+    address[] memory foo; //jeez!
+    _instances.push(attestation_instance(vals,foo,foo));
+  }
+
+
+  //getters
+  function prop_names() public view returns (string[] memory) { return _prop_names; }
+  function instances() public view returns (attestation_instance[] memory) {
+    return _instances;
+  }
+
+
 }
--- test/AttestationStructure.t.sol
+++ test/AttestationStructure.t.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
-import "src/Repository.sol";
+import "src/AttestationStructure.sol";
 
 interface CheatCodes {
     function prank(address) external;
@@ -11,11 +11,31 @@ interface CheatCodes {
     function stopPrank() external;
 }
 
-
 contract AttestationStructureTest is Test {
   CheatCodes cheats = CheatCodes(HEVM_ADDRESS);
+  string[] foo; // Oh boy, does this suck!
+  AttestationStructure s;
   function testPropNames() public {
-
+    foo=["name","date","place"];
+    s=new AttestationStructure(foo);
+    assertEq(3,s.prop_names().length);
+    assert(Utils.compareStrings("name", s.prop_names()[0]));
+    assert(Utils.compareStrings("place", s.prop_names()[2]));
+  }
+  function testInstances() public {
+    testPropNames();
+    s.new_instance(foo);
+    assertEq(1,s.instances().length);
+    assert(Utils.compareStrings("name", s.instances()[0].prop_values[0]));
+    assert(Utils.compareStrings("date", s.instances()[0].prop_values[1]));
+    foo=["name1","date1","place1"];
+    s.new_instance(foo);
+    foo=["name2","date2","place2"];
+    s.new_instance(foo);
+    assertEq(3,s.instances().length);
+    assert(Utils.compareStrings("name1", s.instances()[1].prop_values[0]));
+    assert(Utils.compareStrings("date2", s.instances()[2].prop_values[1]));
+    assert(Utils.compareStrings("place2", s.instances()[2].prop_values[2]));
   }
 
 }
