fbd3d40ca96eb19c2309a762268470c812ab9663
Mike Evron <evronm@gmail.com>
2022-06-06 14:05:39 -0700

    updated deploy script, started changing index.html structure

--- deploy.sh
+++ deploy.sh
@@ -7,4 +7,4 @@ export UTILS_ADDR=`forge create --legacy --rpc-url $RPC_URL --private-key $KEY $
 export UTILS_LIB="$LIB_FILE:Utils:${UTILS_ADDR}"
 export ATTESTATIONS_ADDR=`forge create --legacy --rpc-url $RPC_URL --private-key $KEY $LIB_FILE:Attestations|grep "Deployed to:"|sed "s/Deployed to: //"`
 export ATTESTATIONS_LIB="$LIB_FILE:Utils:${ATTESTATIONS_ADDR}"
-export CONTRACT_ADDR=`forge create --legacy --rpc-url $RPC_URL --private-key $KEY ./src/Repository.sol:Repository --libraries $UTILS_LIB --libraries $KVS_LIB --libraries $STR_ADDRS_LIB --libraries $TAGS_LIB |grep "Deployed to:"|sed "s/Deployed to: //"`
+export CONTRACT_ADDR=`forge create --legacy --rpc-url $RPC_URL --private-key $KEY ./src/Factory.sol:Factory --libraries $UTILS_LIB --libraries $KVS_LIB --libraries $STR_ADDRS_LIB --libraries $TAGS_LIB |grep "Deployed to:"|sed "s/Deployed to: //"`
